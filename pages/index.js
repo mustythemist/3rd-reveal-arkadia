5284c3f5fc6fe92effd9f3a80290b6ec4f5ad641
tharindu <mustythemist@gmail.com>
2023-03-31 01:03:42 +0530

    contract updated

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,33 @@
-import { ConnectWallet, useAddress } from "@thirdweb-dev/react";
+import { ConnectWallet, useAddress, useContract, useOwnedNFTs } from "@thirdweb-dev/react";
+import { useEffect } from "react";
 import CodeSnippet from "../components/DemoCodeSnippet";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
 
+  // const address = useAddress();
+  // Your smart contract address
+  // const contractAddress = "0x830A0C890A1F969586612b1FD98480e9406941f4";
+
+  // const { contract } = useContract(
+  //   "0x3a88e821D5F8cFB1694e9C23BE2CD68B6a3c12B7"
+  // );
+
+
+  // const { data, isLoading, error } = useOwnedNFTs(contract, address);
+  // console.log('data', data);
+  // console.log('isLoading', isLoading);
+  // console.log('error', error);
+
+
   const address = useAddress();
+  const { contract } = useContract('0x3c178321f5BC73494046a46b5A065F9211b7C65E');
+  const { data, isLoading, error } = useOwnedNFTs(
+    contract,
+    address,
+  );
+
+  console.log('data', data);
 
   return (
     <div
@@ -16,6 +39,8 @@ export default function Home() {
       </p> */}
       <h2>Address{address}</h2>
       <ConnectWallet accentColor="#f213a4" colorMode="dark" />
+
+      <h3>NFTs : {data && data.metadata.id}</h3>
 
       <hr className={`${styles.divider} ${styles.spacerTop}`} />
 
